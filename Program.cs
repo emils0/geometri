66781500a3902d03c18f1f2f0e16140d4a930298
neolude <89942504+neolude@users.noreply.github.com>
2021-09-01 20:20:25 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Geometri {
+    public class Square {
+        private int side;
+        public int Side {
+            get { return side; }
+        }
+
+        public Square(int side) {
+            this.side = side;
+        }
+
+        public int Perimiter() {
+            return side * 4;
+        }
+
+        public int Area() {
+            return side * side;
+        }
+    }
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Length of side: ");
+            int input = int.Parse(Console.ReadLine());
+            Square s = new Square(input);
+	    
+            Console.WriteLine($"Perimeter: {s.Perimiter()}");
+            Console.WriteLine($"\nArea: {s.Area()}");
+        }
+    }
+}
